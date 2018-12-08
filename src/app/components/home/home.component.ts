4177eff7757e5a3d68c7f53180774d2c0f37a31e
veronicagr <veronica.gomes.rocha@gmail.com>
2018-12-08 20:15:24 -0200

    correcao de bug img home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,17 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   showInvestimento: boolean = false
+  showOcultarInvestimento: boolean = true
   showSaldo: boolean = false
+  ocultarImgSaldo: boolean = true
   showExtrato: boolean = false
+  showOcultarExtrato: boolean = true
   showTranferencia: boolean = false
+  showOcultaImgTranferencia: boolean = true
   showEmprestimo: boolean = false
+  showOcultarImgEmprestimo: boolean = true
   showPoupanca: boolean = false
-  showOcultarInvestimento: boolean = false
+  showOcultarImgPoupanca: boolean = true
+
   constructor() { }
 
   ngOnInit() {
   }
-
 
   ocultarImagInvestimento(investimento) {
     this.showOcultarInvestimento = !this.showOcultarInvestimento;
@@ -34,10 +39,19 @@ export class HomeComponent implements OnInit {
     this.showSaldo = !this.showSaldo;
     console.log(this.showSaldo)
   }
+  ocultarImgsSaldo(investimento) {
+    this.ocultarImgSaldo = !this.ocultarImgSaldo;
+    console.log(this.ocultarImgSaldo)
+  }
 
   mostrarExtrato(extrato) {
     this.showExtrato = !this.showExtrato;
     console.log(this.showExtrato)
+  }
+
+  ocultarImgExtrato(extrato) {
+    this.showOcultarExtrato = !this.showOcultarExtrato;
+    console.log(this.showOcultarExtrato)
   }
 
   mostrarTransferencia(tranferencia) {
@@ -45,13 +59,27 @@ export class HomeComponent implements OnInit {
     console.log(this.showTranferencia)
   }
 
+  ocultaImgTransferencia(tranferencia) {
+    this.showOcultaImgTranferencia = !this.showOcultaImgTranferencia;
+    console.log(this.showOcultaImgTranferencia)
+  }
+
   mostrarEmprestimo(emprestimo) {
     this.showEmprestimo = !this.showEmprestimo;
     console.log(this.showEmprestimo)
   }
 
+  ocultarImgEmprestimo(emprestimo) {
+    this.showOcultarImgEmprestimo = !this.showOcultarImgEmprestimo;
+    console.log(this.showOcultarImgEmprestimo)
+  }
   mostrarPoupanca(poupanca) {
     this.showPoupanca = !this.showPoupanca;
     console.log(this.showPoupanca)
+  }
+
+  ocultarImgPoupanca(poupanca) {
+    this.showOcultarImgPoupanca = !this.showOcultarImgPoupanca;
+    console.log(this.showOcultarImgPoupanca)
   }
 }
